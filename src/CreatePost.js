b0f3bdbf2a60f6a4d862051720d1c0bf4c60ff29
Kiril <kirilstojanovski00@gmail.com>
2024-07-03 18:20:10 +0200

    add react-quill

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,7 +1,17 @@
-
+import Header from './Header.js'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
 
 export default function CreatePost(){
+    const [value, setValue] = useState('');
     return(
-        <h1>Create post route</h1>
+        <div>
+            <Header />
+            <h1>Create post route</h1>
+            <ReactQuill theme="snow" value={value} onChange={setValue}/>
+            <hr />
+            <div className="ql-editor" dangerouslySetInnerHTML={{__html: value}} />
+        </div>
     );
 }
