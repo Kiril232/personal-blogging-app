16e7bc0eec68043d22f11a5fb8d4f809bd4d76c4
Kiril <kirilstojanovski00@gmail.com>
2024-07-13 17:16:19 +0200

    add cover image preview without uploading to firebase storage

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -3,48 +3,89 @@ import ReactQuill, {Quill} from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useState, useMemo, useRef } from "react";
 import { collection, addDoc, doc } from "firebase/firestore";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { db, storage } from "./firebase.js"
 import  ImageResize   from "quill-image-resize";
 import { useNavigate } from "react-router-dom";
+import ProgressBar from "./ProgressBar";
+
 window.Quill = ReactQuill;
 Quill.register('modules/imageResize', ImageResize);
+
+
 export default function CreatePost(){
-    const [coverImg, setCoverImg] = useState(null);
     const [post, setPost] = useState({
         title: "",
         content: "",
         category: "",
-        date: new Date().toLocaleDateString(),
-        coverImage: ""
-
+        date: new Date().toLocaleDateString()
     });
     const navigate = useNavigate();
     const docRef = collection(db, "posts");
-    const handleSubmit = async (e) => {
-        //TODO: UPLOAD POST TO FIRESTORE
+    const [coverImageInProgress, setCoverImageInProgress] = useState(false);
+    const [imageInProgress, setImageInProgress] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [coverURL, setCoverURL] = useState("");
+
+    const coverImg = useRef(null);
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(post);
-        const newDoc = await addDoc(docRef, post);
+        if(!coverImg){
+            return;
+        }
+        const imageRef = ref(storage, coverImg.current.name + new Date().getTime());
+        const uploadTask =  uploadBytesResumable(imageRef, coverImg.current);
+         uploadTask.on('state_changed',
+            (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                // setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            },
+            // Handle unsuccessful uploads
+            (error) => {
+                console.log(error);
+            },
+            //handle successful finish
+             () => {
+                getDownloadURL(imageRef).then(
+                     (url) => {
+                        const newDoc = addDoc(docRef, {...post, coverImage: url});
+                    })
+            }
+        );
         navigate("/");
     }
 
     const  uploadToStorage =  (image) => {
+        setImageInProgress(true);
         const imageRef = ref(storage, image.name + new Date().getTime());
         // let imageURL = null;
-        uploadBytesResumable(imageRef, image).then(
+        const uploadTask = uploadBytesResumable(imageRef, image);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            },
+            // Handle unsuccessful uploads
+            (error) => {
+                console.log(error);
+            },
+            //handle successful finish
             () => {
                 getDownloadURL(imageRef).then(
                     (url) => {
-                    let selection = quillRef.current.getEditor().getSelection();
-                    quillRef.current.getEditor().insertEmbed(selection.index, "image", url);
-                })
+                        let selection = quillRef.current.getEditor().getSelection();
+                        quillRef.current.getEditor().insertEmbed(selection.index, "image", url);
+                        setImageInProgress(false);
+                        setProgress(0);
+                    })
             }
-        )
+        );
     }
 
     const handleImage = () => {
-        console.log("Slikata e fatena na delo");
         const imageInput = document.createElement("input");
         imageInput.setAttribute("type", "file");
         imageInput.setAttribute("accept", "image/png, image/gif, image/jpeg, image/bmp");
@@ -65,9 +106,9 @@ export default function CreatePost(){
                 ['clean'],
             ],
 
-            // handlers: {
-            //     image: handleImage
-            // },
+            handlers: {
+                image: handleImage
+            },
 
         },
             imageResize: {
@@ -90,45 +131,46 @@ export default function CreatePost(){
     const quillRef = useRef(null);
 
     function handleCoverInput (e) {
-        //TODO: UPLOAD IMAGE TO FIREBASE STORAGE AND PROVIDE PREVIEW, AS WELL AS PROGRESS BAR
-        console.log("Slikata e fatena na delo");
-        const image = e.target.files[0];
-        const imageRef = ref(storage, image.name + new Date().getTime());
-        // let imageURL = null;
-        uploadBytesResumable(imageRef, image).then(
-            () => {
-                getDownloadURL(imageRef).then(
-                    (url) => {
-                        alert("image uploaded")
-                        setPost({...post, coverImage: url});
-                    })
-            }
-        )
-
+        console.log(coverURL);
+        const image = e.target.files && e.target.files[0];
+        if(image){
+            coverImg.current = image;
+        }
+        if(coverURL.length !== 0){
+            URL.revokeObjectURL(coverURL);
+        }
+        if(coverImg.current){
+            setCoverURL(URL.createObjectURL(image));
+        }
     }
 
 
     return(
         <div>
-            <Header />
+            <Header/>
             <h1>Create Post route</h1>
             <form onSubmit={handleSubmit}>
                 <h2>Title:</h2>
-                <input  onChange={(e)=>{setPost({...post, title: e.target.value})}} className="formInput" type="text" placeholder="Enter title..." />
+                <input onChange={(e) => {
+                    setPost({...post, title: e.target.value})
+                }} className="formInput" type="text" placeholder="Enter title..."/>
                 <h2>Category:</h2>
-                <input  onChange={(e)=>{setPost({...post, category: e.target.value})}} className="formInput" type="text" placeholder="Enter category..." />
+                <input onChange={(e) => {
+                    setPost({...post, category: e.target.value})
+                }} className="formInput" type="text" placeholder="Enter category..."/>
                 <h2>Cover image:</h2>
-                <input onInput={handleCoverInput} type="file" />
-                <img src={post.coverImage} width="200" height="200" />
-                <hr />
+                {coverImageInProgress ? <ProgressBar progress={progress} /> : <input onInput={handleCoverInput} type="file"/>}
+                <img src={coverURL} width="200" height="200" alt="cover image"/>
+                <hr/>
+                {imageInProgress && <ProgressBar progress={progress} />}
                 <ReactQuill ref={quillRef} formats={formats} modules={modules} theme="snow" value={post.content}
-                                           onChange={(HTMLcontent) => {
-                                               setPost({...post, content: HTMLcontent});
-                                           }} placeholder="Enter blog content here..."/>
+                            onChange={(HTMLcontent) => {
+                                setPost({...post, content: HTMLcontent});
+                            }} placeholder="Enter blog content here..."/>
                 <button type="submit">Submit</button>
             </form>
-            <hr />
-            <div className="ql-editor ql-container" dangerouslySetInnerHTML={{__html: post.content}} />
+            <hr/>
+            <div className="ql-editor ql-container" dangerouslySetInnerHTML={{__html: post.content}}/>
         </div>
     );
 }
